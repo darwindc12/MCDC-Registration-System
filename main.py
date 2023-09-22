b33aa76249017d4e06a1a1854808ffd08223a53c
Darwin Dela Cruz <darwindelacruz0012@gmail.com>
2023-09-23 06:20:17 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,400 @@
+from PyQt6.QtCore import Qt, QDate
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
+    QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QComboBox, QToolBar, QStatusBar, QMessageBox, QDateEdit
+from PyQt6.QtGui import QAction, QIcon
+import sys
+import sqlite3
+import mysql.connector
+
+
+class DatabaseConnection:
+    def __init__(self, host="localhost", user="root", password="p@ssw0rd_2023", database="registration"):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def connect(self):
+        connection = mysql.connector.connect(host=self.host, user=self.user, password=self.password,
+                                             database=self.database)
+        return connection
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Maasin Day Care Center Registration System")
+        self.setMinimumSize(800, 600)
+
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+        search_menu_item = self.menuBar().addMenu("&Search")
+
+        add_student_action = QAction(QIcon("icons/icons/add.png"), "Add Student", self)
+        add_student_action.triggered.connect(self.insert)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.triggered.connect(self.about)
+
+        search_action = QAction(QIcon("icons/icons/search.png"), "Search", self)
+        search_action.triggered.connect(self.search)
+        search_menu_item.addAction(search_action)
+
+        # Create a toolbar and add toolbar elements
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+
+        # Add a search box (QLineEdit) to the toolbar
+        self.search_input = QLineEdit()
+        toolbar.addWidget(self.search_input)
+
+        # Add a search button (QPushButton) to the toolbar
+        search_button = QPushButton("Search")
+
+        search_button.clicked.connect(self.search)
+        toolbar.addWidget(search_button)
+
+        # Set selection behavior to select entire rows
+
+
+        # Create a table widget and set it as the central widget
+        self.table = QTableWidget()
+
+        self.table.setSelectionBehavior(QTableWidget.SelectionBehavior.SelectRows)
+
+        self.table.setColumnCount(9)
+        self.table.setHorizontalHeaderLabels((
+            "Student ID", "Firstname", "Lastname", "Middlename", "Birthdate", "Gender",
+            "Address", "Guardian", "Mobile Number"))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+
+        # Create a status bar and add status bar elements
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        # Detect a cell click
+        self.table.cellClicked.connect(self.cell_clicked)
+
+
+
+    def cell_clicked(self):
+        edit_button = QPushButton("Edit Record")
+        edit_button.clicked.connect(self.edit)
+
+        delete_button = QPushButton("Delete Record")
+        delete_button.clicked.connect(self.delete)
+
+        children = self.findChildren(QPushButton)
+        if children:
+            for child in children:
+                self.statusbar.removeWidget(child)
+
+        self.statusbar.addWidget(edit_button)
+        self.statusbar.addWidget(delete_button)
+
+    def load_data(self):
+        connection = DatabaseConnection().connect()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM student_info")
+        result = cursor.fetchall()
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+    def search(self):
+        name = self.search_input.text()
+        connection = DatabaseConnection().connect()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM student_info WHERE firstname = %s", (name,))
+        result = cursor.fetchall()
+        rows = list(result)
+        print(rows)
+        items = student_management_sys.table.findItems(name, Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            print(item)
+            student_management_sys.table.item(item.row(), 1).setSelected(True)
+        cursor.close()
+        connection.close()
+
+    # def search(self):
+    #     search_dialog = SearchDialog()
+    #     search_dialog.exec()
+
+    def edit(self):
+        edit_dialog = EditDialog()
+        edit_dialog.exec()
+
+    def delete(self):
+        delete_dialog = DeleteDialog()
+        delete_dialog.exec()
+
+    def about(self):
+        about_dialog = AboutDialog()
+        about_dialog.exec()
+
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        content = """
+        This app was created for learning purposes
+        """
+        self.setText(content)
+
+
+class EditDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Update Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Get student name from selected row
+        index = student_management_sys.table.currentRow()
+        student_firstname = student_management_sys.table.item(index, 1).text()
+        self.student_id = student_management_sys.table.item(index, 0).text()
+
+        # Add student firstname widget
+        self.student_firstname = QLineEdit(student_firstname)
+        self.student_firstname.setPlaceholderText("Firstname")
+        layout.addWidget(self.student_firstname)
+
+        # Add student lastname widget
+        student_lastname = student_management_sys.table.item(index, 2).text()
+        self.student_lastname = QLineEdit(student_lastname)
+        self.student_lastname.setPlaceholderText("Lastname")
+        layout.addWidget(self.student_lastname)
+
+        # Add student middlename widget
+        student_middlename = student_management_sys.table.item(index, 3).text()
+        self.student_middlename = QLineEdit(student_middlename)
+        self.student_middlename.setPlaceholderText("Middlename")
+        layout.addWidget(self.student_middlename)
+
+        # Create a QLabel to serve as a textPlaceholder
+        placeholder_label = QLabel("Birthdate (YYYY-DD-MM):")
+        placeholder_label.setStyleSheet("color: gray;")  # Style it as a placeholder
+        layout.addWidget(placeholder_label)
+
+        # Insert student birthday widget
+        student_birthdate_text = student_management_sys.table.item(index, 4).text()
+        date_format = "yyyy-dd-MM"
+        student_birthdate = QDate.fromString(student_birthdate_text, date_format)
+        self.student_birthdate = QDateEdit(student_birthdate)
+        self.student_birthdate.setCalendarPopup(True)  # Enable the calendar popup
+        self.student_birthdate.setDisplayFormat("yyyy-dd-MM")
+        layout.addWidget(self.student_birthdate)
+
+        # Create a QLabel to serve as a textPlaceholder
+        placeholder_label = QLabel("Gender:")
+        placeholder_label.setStyleSheet("color: gray;")  # Style it as a placeholder
+        layout.addWidget(placeholder_label)
+
+        # Add student gender widget
+        student_gender = student_management_sys.table.item(index, 5).text()
+        self.student_gender = QComboBox()
+        courses = ["Male", "Female"]
+        self.student_gender.addItems(courses)
+        self.student_gender.setCurrentText(student_gender)
+        layout.addWidget(self.student_gender)
+
+        # Add student address widget
+        student_address = student_management_sys.table.item(index, 6).text()
+        self.student_address = QLineEdit(student_address)
+        self.student_address.setPlaceholderText("Address")
+        layout.addWidget(self.student_address)
+
+        # Add student guardian widget
+        student_guardian = student_management_sys.table.item(index, 7).text()
+        self.student_guardian = QLineEdit(student_guardian)
+        self.student_guardian.setPlaceholderText("Guardian")
+        layout.addWidget(self.student_guardian)
+
+        # Add mobile widget
+        student_mobile = student_management_sys.table.item(index, 8).text()
+        self.student_mobile = QLineEdit(student_mobile)
+        self.student_mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.student_mobile)
+
+        # Add a submit button
+        self.button = QPushButton("Update")
+        self.button.clicked.connect(self.update_student)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def update_student(self):
+        try:
+            connection = DatabaseConnection().connect()
+            cursor = connection.cursor()
+            sql = "UPDATE student_info SET firstname = %s, lastname = %s, middlename = %s, birthdate = %s, gender = %s," \
+                "address = %s, guardian = %s, mobileno = %s" \
+                "WHERE pk_studentid = %s"
+            cursor.execute(sql,
+                           (self.student_firstname.text(), self.student_lastname.text(), self.student_middlename.text(),
+                            self.student_birthdate.date().toString("yyyy-MM-dd"), self.student_gender.itemText(self.student_gender.currentIndex()),
+                            self.student_address.text(), self.student_guardian.text(), self.student_mobile.text(), self.student_id))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            QMessageBox.information(self, 'Success', 'Data updated successfully!')
+            student_management_sys.load_data()
+        except Exception as e:
+            QMessageBox.warning(self, 'Error', 'Data edit failed!')
+            print(f"An error occurred: {e}")
+
+
+
+class DeleteDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Delete Student Data")
+
+        layout = QGridLayout()
+        confirmation = QLabel("Are you sure you want to delete?")
+        yes = QPushButton("Yes")
+        no = QPushButton("No")
+
+        layout.addWidget(confirmation, 0, 0, 1, 1)
+        layout.addWidget(yes, 1, 0)
+        layout.addWidget(no, 1, 1)
+        self.setLayout(layout)
+
+        yes.clicked.connect(self.delete_button)
+
+    def delete_button(self):
+        # Get select row index and student_id
+        index = student_management_sys.table.currentRow()
+        student_id = student_management_sys.table.item(index, 0).text()
+
+        connection = DatabaseConnection().connect()
+        cursor = connection.cursor()
+        cursor.execute("DELETE from student_info WHERE pk_studentid = %s", (student_id,))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        student_management_sys.load_data()
+
+        self.close()
+
+        confirmation_widget = QMessageBox()
+        confirmation_widget.setWindowTitle("Success")
+        confirmation_widget.setText("The record was deleted successfully!")
+        confirmation_widget.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(295)
+
+        layout = QVBoxLayout()
+
+        # Add student firstname widget
+        self.student_firstname = QLineEdit()
+        self.student_firstname.setPlaceholderText("Firstname")
+        layout.addWidget(self.student_firstname)
+
+        # Add student lastname widget
+        self.student_lastname = QLineEdit()
+        self.student_lastname.setPlaceholderText("Lastname")
+        layout.addWidget(self.student_lastname)
+
+        # Add student middlename widget
+        self.student_middlename = QLineEdit()
+        self.student_middlename.setPlaceholderText("Middlename")
+        layout.addWidget(self.student_middlename)
+
+        # Create a QLabel to serve as a textPlaceholder
+        placeholder_label = QLabel("Birthdate (YYYY-DD-MM):")
+        placeholder_label.setStyleSheet("color: gray;")  # Style it as a placeholder
+        layout.addWidget(placeholder_label)
+
+        # Insert student birthday widget
+        self.student_birthdate = QDateEdit()
+        self.student_birthdate.setCalendarPopup(True)  # Enable the calendar popup
+        self.student_birthdate.setDisplayFormat("yyyy-dd-MM")
+        layout.addWidget(self.student_birthdate)
+
+        # Create a QLabel to serve as a textPlaceholder
+        placeholder_label = QLabel("Gender:")
+        placeholder_label.setStyleSheet("color: gray;")  # Style it as a placeholder
+        layout.addWidget(placeholder_label)
+
+        # Add student gender widget
+        self.student_gender = QComboBox()
+        courses = ["Male", "Female"]
+        self.student_gender.addItems(courses)
+        layout.addWidget(self.student_gender)
+
+        # Add student address widget
+        self.student_address = QLineEdit()
+        self.student_address.setPlaceholderText("Address")
+        layout.addWidget(self.student_address)
+
+        # Add student guardian widget
+        self.student_guardian = QLineEdit()
+        self.student_guardian.setPlaceholderText("Guardian")
+        layout.addWidget(self.student_guardian)
+
+        # Add mobile widget
+        self.student_mobile = QLineEdit()
+        self.student_mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.student_mobile)
+
+        # Add a submit button
+        self.button.clicked.connect(self.add_student)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        try:
+            firstname = self.student_firstname.text()
+            lastname = self.student_lastname.text()
+            middlename = self.student_middlename.text()
+            birthdate = self.student_birthdate.date().toString("yyyy-MM-dd")
+            gender = self.student_gender.currentText()
+            address = self.student_address.text()
+            guardian = self.student_guardian.text()
+            mobile = self.student_mobile.text()
+
+            connection = DatabaseConnection().connect()
+            cursor = connection.cursor()
+            sql = "INSERT INTO student_info " \
+                  "(firstname, lastname, middlename, birthdate, gender, address, guardian, mobileno) " \
+                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (firstname, lastname, middlename, birthdate, gender, address, guardian, mobile))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            QMessageBox.information(self, 'Success', 'Data inserted successfully!')
+            student_management_sys.load_data()
+        except Exception as e:
+            QMessageBox.warning(self, 'Error', 'Data insertion failed!')
+            print(f"An error occurred: {e}")
+
+
+
+app = QApplication(sys.argv)
+student_management_sys = MainWindow()
+student_management_sys.show()
+student_management_sys.load_data()
+sys.exit(app.exec())
