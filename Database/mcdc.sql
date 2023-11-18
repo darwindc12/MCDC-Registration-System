563db091d93aaeefa79ed921a419f23cf855f567
Darwin Dela Cruz <darwindelacruz0012@gmail.com>
2023-11-18 21:46:04 +0800

    Upload the up-to-date schema for MCDC registration system.

--- Database/mcdc.sql
+++ Database/mcdc.sql
@@ -32,7 +32,7 @@ CREATE TABLE `record_info` (
   PRIMARY KEY (`pk_recordid`),
   KEY `record_info_ibfk_1` (`fk_studentid`),
   CONSTRAINT `record_info_ibfk_1` FOREIGN KEY (`fk_studentid`) REFERENCES `student_info` (`pk_studentid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,8 +41,38 @@ CREATE TABLE `record_info` (
 
 LOCK TABLES `record_info` WRITE;
 /*!40000 ALTER TABLE `record_info` DISABLE KEYS */;
-INSERT INTO `record_info` VALUES (1,10001,'Teacher 1','8:00 AM - 10:00 AM','2024-2025','Enrolled');
 /*!40000 ALTER TABLE `record_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student_info`
+--
+
+DROP TABLE IF EXISTS `student_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_info` (
+  `pk_studentid` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `middlename` varchar(255) DEFAULT NULL,
+  `birthdate` date NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `guardian` varchar(45) NOT NULL,
+  `mobileno` int NOT NULL,
+  PRIMARY KEY (`pk_studentid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10008 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_info`
+--
+
+LOCK TABLES `student_info` WRITE;
+/*!40000 ALTER TABLE `student_info` DISABLE KEYS */;
+INSERT INTO `student_info` VALUES (10001,'Darwin','Dela Cruz','De Los Santos','2021-06-02','Male','Zamboanga City','Darwin Dela Cruz',912345678);
+/*!40000 ALTER TABLE `student_info` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -54,4 +84,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-27  6:28:08
+-- Dump completed on 2023-11-18 21:43:32
